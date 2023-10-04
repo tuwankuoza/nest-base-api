041750e2c29bdbb9157f932c2cd29cdbcf515ce2
tuwankuoza <twkoza@gmail.com>
2023-10-04 09:38:39 +0700

    db connection

---
+++ migrations/20231004022036_users.ts
@@ -0,0 +1,14 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('firstname').notNullable();
+    table.string('lastname').notNullable();
+    table.string('email').notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KnexModule } from 'nest-knexjs';
 
 @Module({
-  imports: [],
+  imports: [
+    KnexModule.forRoot({
+      config: {
+        client: 'postgres',
+        version: '5.7',
+        useNullAsDefault: true,
+        connection: {
+          host: '127.0.0.1',
+          port: 5432,
+          user: 'postgres',
+          password: 'postgres',
+          database: 'test',
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
